d61c15b7caea6f930346c0cc5c90ed3ee93edade
Anda <andzelikawos56@gmail.com>
2023-09-19 15:35:00 +0200

    factorial done poly +-* and +-*/ left

--- Compulsory 2/Compulsory 2.cpp
+++ Compulsory 2/Compulsory 2.cpp
@@ -2,46 +2,75 @@
 #include "Math.h"
 using namespace std;
 
-
-int factorial(int choice);
-int factorial(int n);
+long int factorial(long int n);
+long int function();
 
 int main() 
 {
-	int choice;
-	cout << "1. Factorial\n";
-	cout << "2. 3rd degree polynomial equations\n";
-	cout << "3. Simple math equations\n";
-	cout << "4. Exit\n";
-	cout << "\n";
-	cout << "Please type 1, 2, 3 or 4 to choose option\n";
-	cin >> choice;
-	switch (choice) 
-	{
-	case 1: 
-		factorial(choice);
-		break;
-	default:
-		cout << "whatever";
-	}
+	long int n;
+	unsigned long int result;
+	bool isRunning = true;
 
-	int n;
-	cout << "number\n";
-	cin >> n;
-	cout << "ye\n";
+
+	int choice;
+	while (isRunning == true)
+	{
+		cout << "\n";
+		cout << "Please type 1, 2, 3 or 4 to choose option\n";
+		cout << "1. Factorial\n";
+		cout << "2. 3rd degree polynomial equations\n";
+		cout << "3. Simple math equations\n";
+		cout << "4. Exit\n";
+		cout << "\n";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			cout << "\n";
+			cout << "Type a number you want the factorial of\n";
+			cin >> n;
+			if (n < 0)
+			{
+				cout << "silly, factorial of negative numbers is not defined\n";
+				result = -1;
+			}
+			else
+			{
+				result = factorial(n);
+				cout << "Factorial of " << n << " is:";
+				cout << result;
+				cout << "\n";
+			}
+			break;
+
+		case 2:
+			result = function();
+			cout << result;
+			break;
+
+		case 3:
+			break;
+
+		case 4:
+			isRunning = false;
+			break;
+
+		default:
+			cout << "I SAID 1, 2, 3 OR 4!!!\n";
+			cout << "please try again ^^\n";
+
+
+		}
+	}
 	
 	return 0;
 
 }
 
-int factorial(int n)
+long int factorial(long int n)
 {
-	if (n < 0)
-	{
-		cout << "silly, factorial of negavive numbers is not defined";
-	}
-
-	int result;
+	long int result;
+	
 	if (n == 1 || n == 0)
 	{
 		result = 1;
@@ -50,4 +79,49 @@ int factorial(int n)
 		result = n * factorial(n - 1);
 	}
 	return result;
+}
+
+
+long int function()
+{
+	int a, b, c, d;
+	int j = 3;
+	int firstPoly[4];
+	for (int i = 0; i < 4; i++, j--)
+	{
+		cout << "enter number :)\n";
+		cin >> firstPoly[i];
+		cout << firstPoly[i] << "x^" << j << "\n";
+
+	}
+	cout << "\n";
+	cout << "your poly:\n";
+	int j2 = 3;
+	for (int i = 0; i < 3; i++, j2--)
+	{
+		cout << firstPoly[i] << "x^" << j2 << " + ";
+	}
+	cout << firstPoly[3];
+	cout << "\n";
+	cout << "\n";
+
+	int a2, b2, c2, d2;
+	int k = 3;
+	int secondPoly[4];
+	for (int i = 0; i < 4; i++, k--)
+	{
+		cout << "more numbers :3\n";
+		cin >> secondPoly[i];
+		cout << secondPoly[i] << "x^" << k << "\n";
+	}
+	cout << "\n";
+	cout << "and the second one:\n";
+	int k2 = 3;
+	for (int i = 0; i < 3; i++, k2--)
+	{
+		cout << secondPoly[i] << "x^" << k2 << " + ";
+	}
+	cout << secondPoly[3];
+
+	return 0;
 }
